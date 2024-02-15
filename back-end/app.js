93a0ffa2a98bc534947af03ddddf66f683380d71
AliHamdi63 <aawere1233@gmail.com>
2024-02-15 21:35:15 +0200

    commit

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-
+const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/images', express.static(path.join(__dirname, './assets')));
 
