8f1f6092373c01ae89b5b2f1ab5f90478dabb8e5
AliHamdi63 <aawere1233@gmail.com>
2024-02-15 21:17:31 +0200

    commit

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://localhost:4000'
+    proxy: process.env.BACK_END_URL
   }
 })
