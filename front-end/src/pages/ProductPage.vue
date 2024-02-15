8db73069edaa590d80f889b92ec9d8c3effb1bb5
AliHamdi63 <aawere1233@gmail.com>
2024-02-15 21:45:58 +0200

    commit

--- front-end/src/pages/ProductPage.vue
+++ front-end/src/pages/ProductPage.vue
@@ -20,7 +20,7 @@ export default {
         }
     },
     async created() {
-        const response = await axios.get('/api/products');
+        const response = await axios.get(`https://oilshopback.onrender.com/api/products`);
         const products = response.data;
         this.products = products;
     }
